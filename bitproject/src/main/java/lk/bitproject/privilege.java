f04f152ef8717b5b19a99af4679cff2faca36f37
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-05 22:11:41 +0530

    edited

---
+++ bitproject/src/main/java/lk/bitproject/privilege.java
@@ -0,0 +1,35 @@
+package lk.bitproject;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Table(name = "privilege")
+@Data
+public class privilege {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment
+    private Integer id;
+
+    @Column(name = "sel")
+    @NotNull
+    private Boolean select;
+
+    @Column(name = "inst")
+    @NotNull
+    private Boolean insert;
+
+    @Column(name = "upd")
+    @NotNull
+    private Boolean update;
+
+    @Column(name = "del")
+    @NotNull
+    private Boolean delete;
+
+    //need to add foreign keys
+
+}
