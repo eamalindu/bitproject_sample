3e58a09a1f7dd736905f33d22e7ca24d87306989
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-06 23:03:03 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/PrivilegeController.java
+++ bitproject/src/main/java/lk/bitproject/PrivilegeController.java
@@ -1,7 +1,9 @@
 package lk.bitproject;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 
@@ -10,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/privilege")
 public class PrivilegeController {
 
+    @GetMapping
+    public ModelAndView privilegeUI(){
+        ModelAndView privilegeView = new ModelAndView();
+        privilegeView.setViewName("");
+        return privilegeView;
+    }
 }
