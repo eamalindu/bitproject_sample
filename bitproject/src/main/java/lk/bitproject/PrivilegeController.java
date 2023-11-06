a79ec1b5168eef1ee7fab14dd36daa28ecd5ccfb
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-06 22:59:16 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/PrivilegeController.java
+++ bitproject/src/main/java/lk/bitproject/PrivilegeController.java
@@ -1,4 +1,13 @@
 package lk.bitproject;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+//class level mapping
+// because /employee is common in all Mappings we can declare it as a class level mapping
+@RequestMapping(value = "/privilege")
 public class PrivilegeController {
+
 }
