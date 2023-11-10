77da37d3497b2d67134997c89c84ced5f2ec7afc
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-10 10:23:36 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/UserController.java
+++ bitproject/src/main/java/lk/bitproject/UserController.java
@@ -1,4 +1,23 @@
 package lk.bitproject;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+
+@RequestMapping(value = "/user")
 public class UserController {
+
+
+    @GetMapping
+    public ModelAndView userUI(){
+        ModelAndView userView = new ModelAndView();
+        userView.setViewName("user.html");
+        return userView;
+    }
+
+
+
 }
