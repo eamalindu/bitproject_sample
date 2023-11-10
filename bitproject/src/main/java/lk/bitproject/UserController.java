59ef7f0e86835e290e781b958275bf6f9f81b1db
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-10 10:28:54 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/UserController.java
+++ bitproject/src/main/java/lk/bitproject/UserController.java
@@ -1,15 +1,20 @@
 package lk.bitproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    private UserDAO userDAO;
 
     @GetMapping
     public ModelAndView userUI(){
@@ -17,6 +22,12 @@ public class UserController {
         userView.setViewName("user.html");
         return userView;
     }
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<User> findAll(){
+        return userDAO.findAll();
+    }
+
 
 
 
