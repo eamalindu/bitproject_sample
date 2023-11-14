edc4546384b7f37a28ad0cdc801b6bccbbc39b1f
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-14 09:43:35 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/User/Controller/UserController.java
+++ bitproject/src/main/java/lk/bitproject/User/Controller/UserController.java
@@ -1,5 +1,7 @@
-package lk.bitproject.User;
+package lk.bitproject.User.Controller;
 
+import lk.bitproject.User.Entity.User;
+import lk.bitproject.User.Dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- bitproject/src/main/java/lk/bitproject/User/Dao/UserDAO.java
+++ bitproject/src/main/java/lk/bitproject/User/Dao/UserDAO.java
@@ -1,5 +1,6 @@
-package lk.bitproject.User;
+package lk.bitproject.User.Dao;
 
+import lk.bitproject.User.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO extends JpaRepository<User,Integer> {
