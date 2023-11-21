2a972b4db1770c584ce721c4a131de47e58a7be1
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-21 11:31:57 +0530

    get by nic and get by email added

--- bitproject/src/main/java/lk/bitproject/Employee/Dao/EmployeeDAO.java
+++ bitproject/src/main/java/lk/bitproject/Employee/Dao/EmployeeDAO.java
@@ -4,10 +4,20 @@ import lk.bitproject.Employee.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface EmployeeDAO extends JpaRepository<Employee,Integer> {
 
 
     //native
     @Query(value = "SELECT lpad(max(emp.empnumber)+1,6,0) as empnumber FROM bitproject.employee as emp;",nativeQuery = true)
     String getNextEmpNumber();
+
+    //hql
+    @Query(value = "SELECT e from Employee e where e.nic=?1")
+    Employee getByNIC(String nic);
+
+    //get * info from given email
+    @Query(value = "SELECT e from Employee e where e.email=?1")
+    Employee getByEmail(String email);
 }
