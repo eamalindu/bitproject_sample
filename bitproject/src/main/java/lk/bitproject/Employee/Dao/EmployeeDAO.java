366b575e4adf427e6124b5e3ea51855891a10372
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-21 09:57:48 +0530

    getNextEmpNumber implemented

--- bitproject/src/main/java/lk/bitproject/Employee/Dao/EmployeeDAO.java
+++ bitproject/src/main/java/lk/bitproject/Employee/Dao/EmployeeDAO.java
@@ -2,8 +2,12 @@ package lk.bitproject.Employee.Dao;
 
 import lk.bitproject.Employee.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeDAO extends JpaRepository<Employee,Integer> {
 
 
+    //native
+    @Query(value = "SELECT lpad(max(emp.empnumber)+1,6,0) as empnumber FROM bitproject.employee as emp;",nativeQuery = true)
+    String getNextEmpNumber();
 }
