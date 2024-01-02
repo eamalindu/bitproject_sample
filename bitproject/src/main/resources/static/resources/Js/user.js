4303f79edc5519e13080b3f2e71bcfe7453d53b4
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-02 15:33:46 +0530

    rowPrint edited

--- bitproject/src/main/resources/static/resources/Js/user.js
+++ bitproject/src/main/resources/static/resources/Js/user.js
@@ -102,7 +102,7 @@ const rowPrint = (ob, rowIndex) => {
     newWindow.document.write(
         "<head>" +
         "<link href='resources/style/table-1.css' rel='stylesheet'><link href='resources/bootstrap-5.2.3/css/bootstrap.min.css' rel='stylesheet'>" +
-        "</head><div class='container w-50 mt-4'>" +
+        "</head><div class='container w-75 mt-4'>" +
         "<h3 class='fw-bold'>User Details of "+ ob.employeeId.fullname+"</h3>"+
         "<table class='custom-table small table-responsive-sm border-1 mt-3'>" +
         "<tr>" +
