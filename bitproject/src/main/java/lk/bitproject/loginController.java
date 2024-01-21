f9c67a211d04225818333881634c4000ed685b3f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-21 11:44:05 +0530

    service edited

--- bitproject/src/main/java/lk/bitproject/loginController.java
+++ bitproject/src/main/java/lk/bitproject/loginController.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping(value = "/login")
 public class loginController {
 
-    @GetMapping
+    @GetMapping(value = "/loginfrom")
     public ModelAndView loginUI(){
         ModelAndView loginView = new ModelAndView();
         loginView.setViewName("login.html");
         return loginView;
     }
 
-    @PostMapping
-    public String login(){
-        return "OK";
-    }
 }
