f75550bf7c0af2b99732fa8d5a85c26a914f6cc1
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-21 12:12:15 +0530

    edited

--- bitproject/src/main/java/lk/bitproject/config/WebConfig.java
+++ bitproject/src/main/java/lk/bitproject/config/WebConfig.java
@@ -1,4 +1,12 @@
 package lk.bitproject.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+//make the class into a config class
+@Configuration
+//enabling web security
+@EnableWebSecurity
 public class WebConfig {
 }
--- bitproject/src/main/java/lk/bitproject/service/LogInUserDetailService.java
+++ bitproject/src/main/java/lk/bitproject/service/LogInUserDetailService.java
@@ -1,4 +1,15 @@
 package lk.bitproject.service;
 
-public class LogInUserDetailService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//make the class into a service class
+@Service
+public class LogInUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
